9ed671ce85092b8d8c41328cc4dc31c4e5288783
Omar-1219 <omario9991@gmail.com>
2024-08-25 17:04:02 +0100

    Add files via upload

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (recipeId) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => (recipe.id === updatedRecipe.id ? updatedRecipe : recipe))
+  })),
+}));
